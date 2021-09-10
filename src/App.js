adc8f0d1b1a932a4265d389415b00204855589e4
samstyle321 <princesyal9@gmail.com>
2021-09-10 11:17:12 +0530

    section update

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import Navbar from "./Navbar";
 import Header from "./Header";
+import HowItWorks from "./HowItWorks";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Header />
+      <HowItWorks />
     </>
   );
 };
---
+++ src/HowItWorks.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const HowItWorks = () => {
+  return (
+    <>
+      <section>
+        <div className="work-container container">
+          <h1 className="main-heading text-center">How does it Work</h1>
+          <div className="row">
+            <div className="col-12 col-lg-4 text-center work-container-subdiv">
+              <i className="fas fa-download"></i>
+              <h2 className="sub-heading">Download App</h2>
+              <p className="main-hero-para w-100">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia
+                quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Etquia quis ?{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HowItWorks;
