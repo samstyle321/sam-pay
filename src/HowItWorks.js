96c32906a09080144e1366ba3efe0132c464a61a
samstyle321 <princesyal9@gmail.com>
2021-09-10 12:15:30 +0530

    Work section

--- src/HowItWorks.js
+++ src/HowItWorks.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import workapi from "./Api/WorkApi";
 
 const HowItWorks = () => {
+  const [workData, setworkData] = useState(workapi);
   return (
     <>
       <section>
         <div className="work-container container">
           <h1 className="main-heading text-center">How does it Work</h1>
           <div className="row">
-            <div className="col-12 col-lg-4 text-center work-container-subdiv">
-              <i className="fas fa-download"></i>
-              <h2 className="sub-heading">Download App</h2>
-              <p className="main-hero-para w-100">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia
-                quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Etquia quis ?{" "}
-              </p>
-            </div>
+            {workData.map((curEle) => {
+              const { id, logo, title, info } = curEle;
+              return (
+                <>
+                  <div className="col-12 col-lg-4 text-center work-container-subdiv">
+                    <i className={`fonstawesome-style ${logo}`}></i>
+                    <h2 className="sub-heading">{title}</h2>
+                    <p className="main-hero-para w-100">{info}</p>
+                  </div>
+                </>
+              );
+            })}
           </div>
         </div>
       </section>
