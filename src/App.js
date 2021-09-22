664701552dd8fa353908acd5584d2b74f88d43cb
samstyle321 <princesyal9@gmail.com>
2021-09-22 17:15:43 +0530

    Service Api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./Navbar";
 import Header from "./Header";
 import HowItWorks from "./HowItWorks";
 import AboutUs from "./AboutUs";
+import Services from "./Services";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Header />
       <HowItWorks />
       <AboutUs />
+      <Services />
     </>
   );
 };
