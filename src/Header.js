1b8e84dc05e134b6324b859face06233e8da1b7a
samstyle321 <princesyal9@gmail.com>
2021-10-09 22:38:57 +0530

    header responsive

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
       <header>
         <section className="container main-hero-container">
           <div className="row">
-            <div className="col-12 col-lg-6 header-left-side d-flex justify-content-center flex-column align-items-start">
+            <div className="col-12 col-lg-6 header-left-side d-flex justify-content-center flex-column align-items-start order-lg-first order-last">
               <h1 className="display-2">
                 Online Payment Made <br /> Easy for you.
               </h1>
@@ -20,13 +20,13 @@ const Header = () => {
               <div className="input-group mt-3">
                 <input
                   type="text"
-                  className="rounded-pill  me-3 p-2 form-control-text"
+                  className="rounded-pill    me-3 p-2 form-control-text"
                   placeholder="Enter your Email"
                 />
                 <div className="input-group-button">Get it Now</div>
               </div>
             </div>
-            <div className="col-12 col-lg-6 header-right-side d-flex justify-content-center align-items-center main-herosection-images">
+            <div className="col-12 col-lg-6 header-right-side d-flex justify-content-center align-items-center main-herosection-images order-md-first order-sm-first">
               <img
                 src="./images/hero1.jpg"
                 alt="hero-img"
